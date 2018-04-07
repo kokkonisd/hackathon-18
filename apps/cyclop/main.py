400607c07fa106b0f38c63294c746ca61fa5140b
yleprince <yrieix.leprince@gmail.com>
2018-04-07 21:10:16 +0100

    minor change 2

--- apps/cyclop/main.py
+++ apps/cyclop/main.py
@@ -5,16 +5,16 @@ from cyclop import take_picture
 import sys
 
 for argument in sys.argv[1::]:
-    parsed_arg = argument.split('=')
-    if (len(parsed_arg) >= 1):
-        arg = parsed_arg[0]
-        if (len(parsed_arg) >= 2):
-            value = parsed_arg[1]
-        else:
-            value=None
+	parsed_arg = argument.split('=')
+	if (len(parsed_arg) >= 1):
+		arg = parsed_arg[0]
+	if (len(parsed_arg) >= 2):
+		value = parsed_arg[1]
+	else:
+		value=None
 
-        if (arg=="take_picture"):
-            picture_path = take_picture(name=value)
-	    	generate_html(picture_path)
+	if (arg=="take_picture"):
+		picture_path = take_picture(name=value)
+		generate_html(picture_path)
 sys.stdout.flush()
 
