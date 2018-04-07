b12bdb2815bd357c359561c7c07767f4e97591b0
yleprince <yrieix.leprince@gmail.com>
2018-04-07 22:35:46 +0100

    generate gif

--- apps/gif_generator/main.py
+++ apps/gif_generator/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
+import os
 from time import sleep
 from picamera import PiCamera, Color
 
@@ -21,6 +22,10 @@ def take_pictures(nb_images=5):
 		image_number += 1
 	camera.stop_preview()
 
+def generate_gif():
+	command_str = "convert -delay 100 -loop 0 image*.jpg animation.gif"
+	os.system(command_str)
+	return "./animation.gif"
 
 for argument in sys.argv[1::]:
 	parsed_arg = argument.split('=')
@@ -33,7 +38,8 @@ for argument in sys.argv[1::]:
 
 	if (arg=="take_pictures"):
 		take_pictures(value)
-		#generate_html(picture_path)
+		gif_path = generate_gif()
+		generate_html(gif_path)
 
 
 
