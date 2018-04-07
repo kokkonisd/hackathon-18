e96ad444619dfbfcaa33bbed9e7f9533c17dffb8
yleprince <yrieix.leprince@gmail.com>
2018-04-08 00:55:49 +0100

    changing camera server

--- camera-server/camera-server-test.py
+++ camera-server/camera-server-test.py
@@ -6,6 +6,12 @@ import datetime as dt
 from flask import request
 from flask import Flask, url_for
 
+camera = PiCamera()
+camera.rotation = 180
+camera.annotate_text_size = 92
+camera.annotate_foreground = Color(r=255, g=255, b=255)
+camera.annotate_background = Color(r=0, g=0, b=0)	
+
 # path = "/home/pi/image.png"
 
 # def gif_generate(nb_image):
@@ -20,11 +26,6 @@ from flask import Flask, url_for
 def take_picture(name='image.jpg'):
 	#path='/home/pi/Desktop/'
 	path = "./static/"
-	camera = PiCamera()
-	camera.rotation = 180
-	camera.annotate_text_size = 92
-	camera.annotate_foreground = Color(r=255, g=255, b=255)
-	camera.annotate_background = Color(r=0, g=0, b=0)	
 	camera.annotate_text = "Facebook Hackathon " + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 	camera.start_preview()
@@ -35,7 +36,7 @@ def take_picture(name='image.jpg'):
 	return path + name
 
 app = Flask(__name__)
-@app.route('/gif/<name_image>', methods = ['GET'])
+@app.route('/one_picture/<name_image>', methods = ['GET'])
 def gif(name_image):
 	if request.method == 'GET':
 		return take_picture(name_image)
