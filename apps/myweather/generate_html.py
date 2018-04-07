f51c10748911ade50d8c1402a10be6a451fc7c55
Dimitris Kokkonis <kokkonisd@gmail.com>
2018-04-08 01:04:23 +0200

    Finish weather app

--- apps/myweather/generate_html.py
+++ apps/myweather/generate_html.py
@@ -2,19 +2,19 @@ def get_thermometer(tmp):
     if (tmp>25):
         return "full"
     elif (tmp>10):
-        return "quater"
+        return "quarter"
     else:
         return "empty"
 
-icon={"Rain": "umbrella", "Clouds": "cloud", "Sun": "sun-o"}
+icon={"Clear": "sun-o", "Rain": "umbrella", "Clouds": "cloud", "Sun": "sun-o"}
 def get_icon(weather):
     if (not weather in icon):
-        return "sun-o"
+        return "cloud"
     else:
         return icon[weather]
 
 def generate_html(city_list, path):
-    html="""<!DOCTYPE html>
+    html='''<!DOCTYPE html>
 <html>
 <head>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css" rel="stylesheet" type="text/css" />
@@ -54,12 +54,16 @@ def generate_html(city_list, path):
 
             $(".delete").click(function() {
                 var cityName = $(this).parent().children(".city-name")[0].innerHTML;
-                console.log("delete " + cityName);
+                $.get("/run/myweather/remove="+cityName.toLowerCase());
+                $.get("/apps/myweather/");
+                location.reload();
             });
 
             $(".submitCity").click(function() {
                 var cityName = $("#cityName").val();
-                console.log("add " + cityName);
+                $.get("/run/myweather/add="+cityName.toLowerCase());
+                $.get("/apps/myweather/");
+                location.reload();
             });
 
             loopFlash();
@@ -128,19 +132,19 @@ def generate_html(city_list, path):
             </div>
         </div>
 
-        <hr>"""
+        <hr>'''
 
 
     
 
     for city in city_list:
-        html+="""<div class="row">
+        html+='''<div class="row">
                     <div class="city card col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 mx-auto">
-                        <div class="city-name lead text-center">"""+city["name"]+"""</div>
+                        <div class="city-name lead text-center">'''+city["name"]+'''</div>
                         <br>
                         <ul class="weather-items">
-                            <li><div class="text-center"><i class="fa fa-thermometer-"""+get_thermometer(city["main"]["temp"]-273.15)+"""></i>"""+ str(city["main"]["temp"]-273.15) + """&deg;C</div></li>
-                            <li><div class="text-center"><i class="fa fa-"""+get_icon(city["weather"][0]["main"])+"""></i> """+ city["weather"][0]["main"] +"""</div></li>
+                            <li><div class="text-center"><i class="fa fa-thermometer-'''+get_thermometer(city["main"]["temp"]-273.15)+'''"></i> '''+ str(city["main"]["temp"]-273.15) + '''&deg;C</div></li>
+                            <li><div class="text-center"><i class="fa fa-'''+get_icon(city["weather"][0]["main"])+'''"></i> '''+ city["weather"][0]["main"] +'''</div></li>
                         </ul>
                     <br>
                     <i class="fa fa-times delete"></i>
@@ -148,11 +152,23 @@ def generate_html(city_list, path):
             </div>
 
             <br>
-            """
+            '''
 
-    html+="""</div>
-            </body>
-            </html>"""
+    html+='''<hr>
+
+        <div class="row">
+            <div class="add col-lg-6 col-md-6 col-sm-12 col-xs-12 col-12 mx-auto text-center">
+                <div class="input-group">
+                    <input type="text" id="cityName" class="form-control">
+                    <div class="input-group-append">
+                        <button class="btn btn-secondary submitCity" type="button">Add city</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>'''
 
     file=open(path + "vue.html", "w")
     file.write(html)
