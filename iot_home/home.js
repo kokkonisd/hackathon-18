27c33fe2e55009a0803849fb489870dd9184c17b
Dimitris Kokkonis <kokkonisd@gmail.com>
2018-04-07 14:48:08 +0200

    Change home server code t show what apps are installed

--- iot_home/home.js
+++ iot_home/home.js
@@ -20,13 +20,27 @@ function listening() {
 app.use(bodyParser.urlencoded({ extended : false} ));
 app.use(bodyParser.json());
 
+var app_list = JSON.parse(fs.readFileSync("apps/apps.json"));
+
+app.get("/apps/", listApps);
+function listApps(request, response) {
+    var reply = "App Name\tVersion\n";
+    for (var key in app_list) {
+        reply += app_list[key].name + "\t" + app_list[key].version + "\n";
+    }
+    console.log(reply);
+    response.send(reply);
+}
+
 
 function searchApps (name) {
     http.get(`http://localhost:8080/search/${name}`, getAppList);
 }
 
 function getAppList (data) {
+    console.log(`====\nHere's the app list :\n----`);
     console.log(data);
+    console.log("====");
 }
 
 function downloadApp(name) {
