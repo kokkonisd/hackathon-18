d6b5e126734b371bc40e5204412d0d67de2c2f20
yleprince <yrieix.leprince@gmail.com>
2018-04-08 04:29:01 +0100

    enabling gif

--- camera-server/camera-server-test.py
+++ camera-server/camera-server-test.py
@@ -1,6 +1,7 @@
 from picamera import PiCamera, Color
 from time import sleep
 import datetime as dt
+import numpy as np
 
 
 from flask import request
@@ -25,9 +26,40 @@ def take_picture(name='image.jpg'):
 	
 	return path + name
 
+
+def take_gif(number_images):
+	#path='/home/pi/Desktop/'
+
+	unique_id = ''.join([str(np.random.randint(10)) for i in range (20)])
+
+	path = "static/" + unique_id
+	camera.annotate_text = "Facebook Hackathon " + dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+	camera.start_preview()
+	
+
+
+	for ID in range(number_images):
+		sleep(1)
+	    image_name = 'image{0:04d}.jpg'.format(ID)
+	    camera.capture(path + image_name)
+	
+	camera.stop_preview()
+	
+	gif_name = path + "animation.gif"
+	command_str = "convert -delay 50 -loop 0" + path + "image*.jpg " + gif_name
+	os.system(command_str)
+
+	return gif_name
+
+
 app = Flask(__name__)
 @app.route('/one_picture/<name_image>', methods = ['GET'])
 def gif(name_image):
 	if request.method == 'GET':
 		return take_picture(name_image)
 
+@app.route('/one_gif/<number_images>', methods = ['GET'])
+def gif(number_images):
+	if request.method == 'GET':
+		return take_gif(number_images)
