173595fed590d4d0bea411bdd93e3f10a86c6cba
CharlyCast <charly.castes@polytechnique.edu>
2018-04-08 02:48:23 +0200

    updating onenode

--- apps/onenode/get_ip.py
+++ apps/onenode/get_ip.py
@@ -5,7 +5,7 @@ def ip_by_name(device_name):
     """return a dictionary that contain the name, ip and type"""
     r=requests.get("http://localhost:5000/getdeviceip?devicename="+device_name)
     devices=json.loads(r.text)
-    return (r.text)
+    return (devices)
 
 def ip_by_signal(signal):
     """return a dictionary that contain the name, ip and type"""
