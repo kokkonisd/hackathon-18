7317b3b80553029d86d333fc0de478d165fc3a7c
yleprince <yrieix.leprince@gmail.com>
2018-04-08 05:32:40 +0100

    more values for cpu temp

--- apps/raspi_cpu_temp/add_value.py
+++ apps/raspi_cpu_temp/add_value.py
@@ -4,12 +4,22 @@ import time
 
 command_str = "/opt/vc/bin/vcgencmd measure_temp"
 
-temp = os.popen(command_str).read()
-t = temp.split('=')[1]
-t = t.split("'")[0]
 
-measure = time.strftime('%X %x %Z') + ";" + str(t) + "\n"
 
-with open('/home/pi/hackathon-18/apps/raspi_cpu_temp/cpu_temp.csv','a') as f:
-        f.write(str(measure))
+def get_measure():
+	temp = os.popen(command_str).read()
+	t = temp.split('=')[1]
+	t = t.split("'")[0]
 
+	measure = time.strftime('%X %x %Z') + ";" + str(t) + "\n"
+
+	with open('/home/pi/hackathon-18/apps/raspi_cpu_temp/cpu_temp.csv','a') as f:
+	        f.write(str(measure))
+
+get_measure()	# 00
+time.sleep(15)
+get_measure()	# 15
+time.sleep(15)
+get_measure()	# 30
+time.sleep(15)
+get_measure()	# 45
